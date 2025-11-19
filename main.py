c0bef9bbce163e2a242329adee5b11031490fb93
Patryk Palej <patrykpalej@proton.me>
2025-11-19 11:30:58 +0100

    2.3 [W] Metoda POST, request body, pydantic

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
+
+class TaskBody(BaseModel):
+    description: str
+    priority: int
+    is_completed: bool
+
+
 tasks_data = [
-    {"description": "Learn FastAPI", "priority": 3, "is_completed": True},
-    {"description": "Do exercises", "priority": 2, "is_completed": False}
+    {"id": 1, "description": "Learn FastAPI", "priority": 3, "is_completed": True},
+    {"id": 2, "description": "Do exercises", "priority": 2, "is_completed": False}
+]
+
+users_data = [
+    {"id": 1, "username": "Andrzej", "password": "qwerty123", "is_admin": True},
+    {"id": 2, "username": "Andżela", "password": "hasło1!", "is_admin": False}
 ]
 
 
@@ -17,3 +30,19 @@ def root():
 @app.get("/tasks")
 def get_tasks():
     return {"result": tasks_data}
+
+
+@app.post("/tasks")
+def create_task(body: TaskBody):
+    new_task = body.model_dump()
+    new_task_id = max(task["id"] for task in tasks_data) + 1
+    new_task["id"] = new_task_id
+    tasks_data.append(new_task)
+
+    return {"message": "New task added", "details": new_task}
+
+
+@app.get("/users")
+def get_users():
+    return {"result": users_data}
+
