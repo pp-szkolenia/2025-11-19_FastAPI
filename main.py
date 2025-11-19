3c459d8754b68983205abe84b34fb6e93fbbe375
Patryk Palej <patrykpalej@proton.me>
2025-11-19 12:29:44 +0100

    2.6 [W] JSONResponse, domyślny status_code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 
@@ -39,7 +40,8 @@ def root():
 
 @app.get("/tasks")
 def get_tasks():
-    return {"result": tasks_data}
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"result": tasks_data})
+
 
 @app.get("/tasks/{task_id}")
 def get_task_by_id(task_id: int):
@@ -50,7 +52,7 @@ def get_task_by_id(task_id: int):
     return {"result": target_task}
 
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
 def create_task(body: TaskBody):
     new_task = body.model_dump()
     new_task_id = max(task["id"] for task in tasks_data) + 1
